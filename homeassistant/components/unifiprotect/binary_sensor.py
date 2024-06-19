0d14653043954e05432e5554635faf6a22d40809
J. Nick Koston <nick@koston.org>
2024-06-19 08:43:20 -0500

    Improve performance of unifiprotect attr handling

--- homeassistant/components/unifiprotect/binary_sensor.py
+++ homeassistant/components/unifiprotect/binary_sensor.py
@@ -634,9 +634,8 @@ class ProtectDeviceBinarySensor(ProtectDeviceEntity, BinarySensorEntity):
     def _async_update_device_from_protect(self, device: ProtectModelWithId) -> None:
         super()._async_update_device_from_protect(device)
         was_on = self.is_on
-        is_on = self.entity_description.get_ufp_value(self.device) is True
-        if was_on != is_on:
-            self._attr_is_on = is_on
+        if was_on != (on := self.entity_description.get_ufp_value(device) is True):
+            self._attr_is_on = on
 
 
 class MountableProtectDeviceBinarySensor(ProtectDeviceBinarySensor):
